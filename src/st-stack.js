8c3ba93b0fb88e7f001d90feabe965e5e0e19164
Yuliya <yuliya.rostova@gmail.com>
2021-10-18 20:36:21 +0300

    feat: add stack class

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.stackArray = [];
+  }
+
+  push(value) {
+    this.stackArray.push(value);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackArray.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackArray[this.stackArray.length - 1];
   }
 }
